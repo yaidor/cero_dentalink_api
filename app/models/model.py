62aeea3c2c3a9827701ec243bd0afb67f66c4f30
yaidor <matias.venegas93@gmail.com>
2024-02-15 11:07:06 -0300

    Se realiza put correctamente

--- app/models/model.py
+++ app/models/model.py
@@ -3,7 +3,12 @@ from datetime import date
 from typing import Optional
 
 class AppointmentModel(BaseModel):
-    fecha_inicio: Optional[date] = None
-    fecha_fin: Optional[date] = None
-    id_sucursales: Optional[list[int]] = None
-    id_estado_cita: Optional[list[int]] = None
+    fecha_inicio: date | None = None
+    fecha_fin: date | None = None
+    id_sucursales: list[int] | None = None
+    id_estado_cita: list[int] | None = None
+
+class StateAppointmentModel(BaseModel):
+    id_cita: int = Field(..., description="Id de la cita")
+    id_estado: int = Field(..., description="Estado de la cita")
+    comentarios: str = Field(..., description="Comentarios del cambio de estado")
--- app/resources/dentalink.py
+++ app/resources/dentalink.py
@@ -1,12 +1,11 @@
 import logging
 import requests
 import json
-
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from datetime import datetime
-from app.models.model import AppointmentModel
+from app.models.model import AppointmentModel, StateAppointmentModel
 from app.config import settings
 
 router = APIRouter(
@@ -17,8 +16,7 @@ router = APIRouter(
 @router.get("/citas")
 async def citas(Data: AppointmentModel = None) -> JSONResponse:
     Appointments = []
-    logging.info("Requesting citas")
-    logging.info(Data)
+    logging.info("Requesting appointments...")
     headers = {
         'Authorization': 'Token ' + settings.app_token
     }
@@ -33,18 +31,22 @@ async def citas(Data: AppointmentModel = None) -> JSONResponse:
         Appointments = Appointments + response["data"]
     else:
         query = {}
-
-        logging.info(Data)
+        logging.info("Querying appointments...")
         if Data.fecha_inicio:
+            logging.info("Has date")
+            logging.debug(f'Fecha inicio: {Data.fecha_inicio}')
             if Data.fecha_fin:
+                logging.debug(f'Fecha fin: {Data.fecha_fin}')
                 query["fecha"] = [{"gte": Data.fecha_inicio.strftime("%Y-%m-%d")}, {"lte": Data.fecha_fin.strftime("%Y-%m-%d")}]
             else:
                 query["fecha"] = {"eq": Data.fecha_inicio.strftime("%Y-%m-%d")}
         if Data.id_estado_cita:
+            logging.info("Has state(s)")
+            logging.debug(f'Estados: {Data.id_estado_cita}')
             for ide in Data.id_estado_cita:
                 query["id_estado"] = {"eq": ide}
                 url = settings.url_citas+'?q='+json.dumps(query)
-                logging.info(url)
+                logging.debug(f'URL id estado: {url}')
                 response = requests.get(url, headers=headers)
                 if response.status_code != 200:
                     logging.error(response.text)
@@ -54,22 +56,41 @@ async def citas(Data: AppointmentModel = None) -> JSONResponse:
             if Data.id_sucursales:
                 Appointments = [cita for cita in Appointments if cita["id_sucursal"] in Data.id_sucursales]
         else:
+            logging.info("No states")
             url = settings.url_citas+'?q='+json.dumps(query)
-            logging.info(url)
+            logging.debug(f'URL: {url}')
             response = requests.get(url, headers=headers)
             if response.status_code != 200:
                 logging.error(response.text)
                 return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error": "Error al obtener citas"})
             response = json.loads(response.text)
             Appointments = Appointments + response["data"]
-        if not Data.fecha_inicio and not Data.id_estado_cita and Data.id_sucursales:
-            url = settings.url_citas
-            response = requests.get(url, headers=headers)
-            if response.status_code != 200:
-                logging.error(response.text)
-                return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error": "Error al obtener citas"})
-            response = json.loads(response.text)
-            Appointments = Appointments + response["data"]
+        if Data.id_sucursales:
             Appointments = [cita for cita in Appointments if cita["id_sucursal"] in Data.id_sucursales]
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(Appointments))
+
+@router.put("/estado_citas")
+async def change_state_appointments(State:StateAppointmentModel = None) -> JSONResponse:
+    
+    if not State:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"error": "No se ha enviado el estado de la cita"})
+    logging.info("Changing state of appointment...")
+    logging.debug(f'ID Cita: {State.id_cita}')
+    logging.debug(f'ID Estado: {State.id_estado}')
+    logging.debug(f'Comentarios: {State.comentarios}')
+    headers = {
+        'Authorization': 'Token ' + settings.app_token
+    }
+    url = settings.url_citas +'/'+ str(State.id_cita)
+    logging.debug(f'URL: {url}')
+    data = {
+        "id_estado": State.id_estado,
+        "comentarios": State.comentarios
+    }
+    logging.debug(f'Data: {data}')
+    response = requests.put(url, headers=headers, data=data)
+    if response.status_code != 200:
+        logging.error(response.text)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error": "Error al cambiar estado de cita"})
+    return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(response.json()))
